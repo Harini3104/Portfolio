d8715d9d24ce5adf3265578d3cba1f536cbb3414
Harini3104 <harinimk3104@gmail.com>
2025-09-02 23:56:16 +0530

    update

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('projects/', views.projects, name='projects'),
     path("education/", views.education_certifications, name="education"),
     path('contact/', views.contact, name='contact'),
-    path("favicon.ico",RedirectView.as_view(url=staticfiles_storage.url("image/favicon.ico"), permanent=True),),]
+   ]
